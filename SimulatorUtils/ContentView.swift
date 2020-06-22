77d1a8dfd0e708298c232a19d19bf34bd1a1d8d9
Nikola Gorgiev <nikolagorgievmkd@gmail.com>
2020-06-22 11:40:44 +0200

    Initial Commit

---
+++ SimulatorUtils/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SimulatorUtils
+//
+//  Created by Nikola Gorgiev on 6/22/20.
+//  Copyright © 2020 Nikola Gorgiev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
